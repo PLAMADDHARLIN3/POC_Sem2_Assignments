6bed3cecd111a71e39ae4f876d42ef9106dbd825
Anthony Harlin <harliant001@warren.k12.in.us>
2024-01-25 12:01:11 -0500

    assignment

--- 03_Generators and Exceptions/03_Exception Hierarchy/assignment.py
+++ 03_Generators and Exceptions/03_Exception Hierarchy/assignment.py
@@ -2,19 +2,15 @@ number1 = 0
 number2 = 0
 try:
     number1 = int(input("Enter a number"))
-    # YOUDO.  use input function and int to set number2
+    number2 = int(input("Enter another number"))
 except ValueError:
-    # print message stating that an integer wasn't given
-    pass #YOUDO remove pass when done
+    print("integer was not given")
 
 
 try:
-    #YOUDO divide number1 / number2 and set to answer
-    #YOUDO  print the result of the division (aka answer with some helper text)
-    pass  # YOUDO remove pass when done
-
+    value = number1/number2
+    print('The answer is', value) 
 except ZeroDivisionError:
-    #YOUDO:  print message stating that division by zero is not possible.  
-    pass  # YOUDO remove pass when done
+    print("the division is not possible")
 
     
