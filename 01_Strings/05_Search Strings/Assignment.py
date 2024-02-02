56992567d068a3cb09e9833324e8c99a70af0be3
Anthony Harlin <harliant001@warren.k12.in.us>
2024-02-02 12:01:44 -0500

    assignment

--- 01_Strings/05_Search Strings/Assignment.py
+++ 01_Strings/05_Search Strings/Assignment.py
@@ -1,4 +1,4 @@
-text = "" #use input to actually store into text.  remove the ""
+text = input("Enter some text") 
 
 result = text.find("the")
 
@@ -6,5 +6,4 @@ if result == -1:
     print("The word the is not in the string")
 else:
     print("The word the is in the string")
-    print("It is located at", )
-    #YOUDO.  print out where the word 'the' appears in the string.  
+    print("It is located at",result)
