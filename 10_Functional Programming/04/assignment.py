f327d37b1773d23d69df6da093cf0b7d185adb96
Anthony Harlin <harliant001@warren.k12.in.us>
2024-04-22 12:28:50 -0400

    connect four

--- 10_Functional Programming/04/assignment.py
+++
@@ -1,8 +0,0 @@
-try:
-    stream = open(
-        "C:\\Users\\harliant001\\POC_Sem2_Assignments\\10_Functional Programming\\04\\assignment.py")
-    stream.write('This is the first sentence\n')
-    stream.close()
-except Exception as e:
-    print('An error occurred:', e)
-    
